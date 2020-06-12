55f6acd94be3235c4112cefe687f47d732c44c78
sourav seal <souravseal.sourav@gmail.com>
2020-06-12 21:58:20 +0530

    done

--- src/app/admin/Hospital/hospital-dashboard/chart/chart.component.ts
+++ src/app/admin/Hospital/hospital-dashboard/chart/chart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { BedModify } from './../../../model/model';
 
 @Component({
   selector: 'app-chart',
@@ -7,19 +8,26 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ChartComponent implements OnInit {
 @Input() bedList: any;
-modifyBedlist: any;
-modBed = [];
+modifyBedlist : any;
+myBed = [];
   constructor() { }
 
   ngOnInit() {
 
   }
   display() {
-    this.modifyBedlist = this.bedList;
-    console.log(this.modifyBedlist);
-    const general = [['general', 2 , 5 ], ['icu', 4 , 7 ], ['general',  1 , 6 ], ['general',  1 , 6 ]];
-    console.log(general);
 
+        this.modifyBedlist = JSON.parse(JSON.stringify(this.bedList));
+        console.log(this.modifyBedlist);
+        this.modifyBedlist.bedModify.forEach(element => {
+          console.log(typeof(element));
+          let arr: Array<any>=[];
+          arr.push(element.bedType);
+          arr.push(element.existingBed);
+          arr.push(element.quantity);
+          this.myBed.push(arr) ;
+        }); 
+        console.log( this.myBed,"he");
   }
 
     //  title = 'googleChartsApp';n
@@ -27,11 +35,13 @@ modBed = [];
 
  general = [['general', 2 , 5 ], ['icu', 4 , 7 ], ['general',  1 , 6 ], ['general',  1 , 6 ]];
  // tslint:disable-next-line: member-ordering
+  
+
  title = 'Population (in millions)';
   // tslint:disable-next-line: member-ordering
   type = 'BarChart';
   // tslint:disable-next-line: member-ordering
-  data = this.modifyBedlist ;
+  // data = [[this.myBed['bedtype'],this.myBed['existingBed'],this.myBed['quantity']  ] ;
   // tslint:disable-next-line: member-ordering
   columnNames = ['Year', 'Occupied', 'Free'];
   // tslint:disable-next-line: member-ordering
@@ -44,6 +54,8 @@ modBed = [];
      },
      isStacked: true
   };
+  data=this.myBed;
+ 
   // tslint:disable-next-line: member-ordering
   width = 500;
   // tslint:disable-next-line: member-ordering
