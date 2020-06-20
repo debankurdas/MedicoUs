113de057d48e8889cdb4af509200d79a2f2c69f7
sourav seal <souravseal.sourav@gmail.com>
2020-06-20 23:00:23 +0530

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -38,6 +38,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProductComponent } from './user/product/product.component';
 import { ProductDetailsComponent } from './user/product/product-details/product-details.component';
 import { StrideComponent } from './user/payment/stride/stride.component';
+import { HospViewComponent } from './user/hospital/hosp-view/hosp-view.component';
 
 
 const routes: Routes = [
@@ -81,6 +82,7 @@ const routes: Routes = [
   { path: 'user/orderDetails', component: OrderDetailsComponent},
   {path: 'user/payWithCard', component: StrideComponent, canActivate: [AuthGuard, UserGuard]},
   { path: 'user/payment', component: PaymentComponent, canActivate: [AuthGuard, UserGuard] },
+  { path: 'user/hospital', component: HospViewComponent, canActivate: [AuthGuard, UserGuard] },
   {path: '', redirectTo: 'index', pathMatch: 'full'}
 ];
 
