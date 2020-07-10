eb6cbd470da2ab7e02ef697b66502d427f1d2725
sourav seal <souravseal.sourav@gmail.com>
2020-07-10 20:35:51 +0530

    done

--- backend/models/bloodBankModels/blooBankSchema.js
+++ backend/models/bloodBankModels/blooBankSchema.js
@@ -2,55 +2,55 @@ const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
 const uniqueValidator = require('mongoose-unique-validator');
 const bloodBankSchema = new mongoose.Schema({
-  bloodBankName: {
-    type: String,
-    required: true
-  },
-  adminId: {
-    type: ObjectId,
-    required: true
-  },
-  imageUrl: {
-    type: String,
-    required: true
-  },
-  status: {
-    type: String,
-    default: 'Active'
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true
-  },
-  mobileNo: {
-    type: String,
-    required: true
-  },
-  state: {
-    type: String,
-    required: true
-  },
-  city: {
-    type: String,
-    required: true
-  },
-  branchArea: {
-    type: String,
-    required: true
-  },
-  address: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  pin: {
-    type: String,
-    required: true
-  }
+    bloodBankName: {
+        type: String,
+        required: true
+    },
+    adminId: {
+        type: ObjectId,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        default: 'Active'
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    mobileno: {
+        type: String,
+        required: true
+    },
+    state: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    branchArea: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    pin: {
+        type: String,
+        required: true
+    }
 
 });
 
